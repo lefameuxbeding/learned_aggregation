3af3e0b614bb1fd0ad94d4f2df7e4688805e9db1
Charles-Étienne Joseph <me@charlesetiennejoseph.net>
2023-02-08 11:01:47 -0500

    formatting

--- plot.py
+++ plot.py
@@ -14,7 +14,11 @@ if __name__ == "__main__":
 
         plt.plot(results["losses_mean"], label=optimizer, color=color)
         plt.fill_between(
-            np.arange(10), results["losses_mean"] - results["losses_std"], results["losses_mean"] + results["losses_std"], alpha=0.1, color=color
+            np.arange(10),
+            results["losses_mean"] - results["losses_std"],
+            results["losses_mean"] + results["losses_std"],
+            alpha=0.1,
+            color=color,
         )
 
     plt.ylim(1.0, 2.4)
