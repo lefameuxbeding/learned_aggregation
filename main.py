67a30c018159dc6a089c84456f94a5d24a493e3d
charles-etienne.joseph <charles-etienne.joseph@login-2.server.mila.quebec>
2023-03-05 18:14:26 -0500

    benchmarking setup

--- main.py
+++ main.py
@@ -8,34 +8,49 @@ from learned_optimization.optimizers import base as opt_base
 
 
 if __name__ == "__main__":
+
+    """Environment"""
+
     sys.path.append(os.getcwd())
     os.environ["TFDS_DATA_DIR"] = os.getenv("SLURM_TMPDIR")
 
+    """Setup"""
+
+    num_runs = 10
+    num_inner_steps = 10
+
     key = jax.random.PRNGKey(0)
     task = image_mlp.ImageMLP_FashionMnist8_Relu32()
-    opt = opt_base.Adam(1e-2)
+
+    """Adam"""
+
+    adam = opt_base.Adam(1e-2)
 
     @jax.jit
-    def update(opt_state, key, batch):
+    def adam_update(opt_state, key, batch):
         key, key1 = jax.random.split(key)
-        params, _ = opt.get_params_state(opt_state)
+        params, _ = adam.get_params_state(opt_state)
         loss, grads = jax.value_and_grad(task.loss)(params, key1, batch)
-        opt_state = opt.update(opt_state, grads, loss=loss)
+        opt_state = adam.update(opt_state, grads, loss=loss)
 
         return opt_state, key, loss
 
-    for j in range(10):
-        run = wandb.init(project="learned_aggregation", group="Adam")
+    """Benchmarking"""
 
-        params = task.init(key)
-        opt_state = opt.init(params)
+    optimizers = [("Adam", adam, adam_update)]
 
-        for i in range(10):
-            batch = next(task.datasets.train)
-            opt_state, key, train_loss = update(opt_state, key, batch)
-            test_loss = task.loss(
-                opt.get_params_state(opt_state)[0], key, next(task.datasets.test)
-            )
-            run.log({"train loss": train_loss, "test loss": test_loss})
+    for opt_str, opt, update in optimizers:
 
-        run.finish()
+        for j in range(num_runs):
+            run = wandb.init(project="learned_aggregation", group=opt_str)
+
+            params = task.init(key)
+            opt_state = opt.init(params)
+
+            for i in range(num_inner_steps):
+                batch = next(task.datasets.train)
+                opt_state, key, train_loss = update(opt_state, key, batch)
+                
+                run.log({"train loss": train_loss})
+
+            run.finish()
