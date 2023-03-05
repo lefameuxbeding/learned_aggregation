c0a49bdba5236a3274ab54a4c8b8e96844f993aa
charles-etienne.joseph <charles-etienne.joseph@login-2.server.mila.quebec>
2023-03-05 16:52:19 -0500

    implemented Adam

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import jax
 import wandb
 
 from learned_optimization.tasks.fixed import image_mlp
+from learned_optimization.optimizers import base as opt_base
 
 
 if __name__ == "__main__":
@@ -12,20 +13,29 @@ if __name__ == "__main__":
 
     key = jax.random.PRNGKey(0)
     task = image_mlp.ImageMLP_FashionMnist8_Relu32()
-    grad_fn = jax.jit(jax.value_and_grad(task.loss))
-    lr = 0.1
+    opt = opt_base.Adam(1e-2)
+
+    @jax.jit
+    def update(opt_state, key, batch):
+        key, key1 = jax.random.split(key)
+        params, _ = opt.get_params_state(opt_state)
+        loss, grads = jax.value_and_grad(task.loss)(params, key1, batch)
+        opt_state = opt.update(opt_state, grads, loss=loss)
+
+        return opt_state, key, loss
 
     for j in range(10):
-        run = wandb.init(project="learned_aggregation", group="SGD")
+        run = wandb.init(project="learned_aggregation", group="Adam")
 
         params = task.init(key)
+        opt_state = opt.init(params)
 
-        for i in range(100):
-            key, key1 = jax.random.split(key)
+        for i in range(10):
             batch = next(task.datasets.train)
-            l, grads = grad_fn(params, key1, batch)
-            params = jax.tree_util.tree_map(lambda p, g: p - lr * g, params, grads)
-            test_l = task.loss(params, key, next(task.datasets.test))
-            run.log({"train loss": l, "test loss": test_l})
+            opt_state, key, train_loss = update(opt_state, key, batch)
+            test_loss = task.loss(
+                opt.get_params_state(opt_state)[0], key, next(task.datasets.test)
+            )
+            run.log({"train loss": train_loss, "test loss": test_loss})
 
         run.finish()
