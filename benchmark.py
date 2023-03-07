fa6d483d855db2e4ba236e7711db0a0b2b4df4d4
charles-etienne.joseph <charles-etienne.joseph@cn-b001.server.mila.quebec>
2023-03-07 12:17:23 -0500

    backbone for learned aggregator

--- benchmark.py
+++ benchmark.py
@@ -8,6 +8,7 @@ from learned_optimization.optimizers import base as opt_base
 from learned_optimization.learned_optimizers import mlp_lopt
 
 from meta_train import meta_train
+from mlp_lagg import MLPLAgg
 
 
 if __name__ == "__main__":
@@ -19,55 +20,74 @@ if __name__ == "__main__":
     """Setup"""
 
     num_runs = 10
-    num_inner_steps = 500
-    num_outer_steps = 10000
+    num_inner_steps = 100
+    num_outer_steps = 2000
 
     key = jax.random.PRNGKey(0)
     task = image_mlp.ImageMLP_FashionMnist8_Relu32()
 
     """Adam"""
 
-    adam = opt_base.Adam(1e-2)
+    # adam = opt_base.Adam(1e-2)
 
-    @jax.jit
-    def adam_update(opt_state, key, batch):
-        key, key1 = jax.random.split(key)
-        params = adam.get_params(opt_state)
-        loss, grads = jax.value_and_grad(task.loss)(params, key1, batch)
-        opt_state = adam.update(opt_state, grads, loss=loss)
+    # @jax.jit
+    # def adam_update(opt_state, key, batch):
+    #     key, key1 = jax.random.split(key)
+    #     params = adam.get_params(opt_state)
+    #     loss, grads = jax.value_and_grad(task.loss)(params, key1, batch)
+    #     opt_state = adam.update(opt_state, grads, loss=loss)
 
-        return opt_state, key, loss
+    #     return opt_state, key, loss
 
     """Per Parameter MLP Optimizer"""
 
-    per_param_mlp_lopt = mlp_lopt.MLPLOpt()
-    per_param_mlp_opt_str = "PerParamMLPOpt"
+    # per_param_mlp_lopt = mlp_lopt.MLPLOpt()
+    # per_param_mlp_opt_str = "PerParamMLPOpt"
+
+    # key, key1 = jax.random.split(key)
+    # per_param_mlp_meta_params = meta_train(
+    #     per_param_mlp_lopt,
+    #     per_param_mlp_opt_str,
+    #     key1,
+    #     num_inner_steps,
+    #     num_outer_steps,
+    # )
+
+    # per_param_mlp_opt = per_param_mlp_lopt.opt_fn(per_param_mlp_meta_params)
+
+    # @jax.jit
+    # def per_param_mlp_opt_update(opt_state, key, batch):
+    #     key, key1 = jax.random.split(key)
+    #     params = per_param_mlp_opt.get_params(opt_state)
+    #     loss, grads = jax.value_and_grad(task.loss)(params, key1, batch)
+    #     opt_state = per_param_mlp_opt.update(opt_state, grads, loss=loss)
+
+    #     return opt_state, key, loss
+
+    """Per Parameter MLP Aggregator"""
+
+    per_param_mlp_lagg = MLPLAgg()
+    per_param_mlp_agg_str = "PerParamMLPAgg"
 
     key, key1 = jax.random.split(key)
-    per_param_mlp_meta_params = meta_train(
-        per_param_mlp_lopt,
-        per_param_mlp_opt_str,
-        key1,
-        num_inner_steps,
-        num_outer_steps,
-    )
 
-    per_param_mlp_opt = per_param_mlp_lopt.opt_fn(per_param_mlp_meta_params)
+    per_param_mlp_agg = per_param_mlp_lagg.opt_fn(per_param_mlp_lagg.init(key1))
 
     @jax.jit
-    def per_param_mlp_opt_update(opt_state, key, batch):
+    def per_param_mlp_agg_update(opt_state, key, batch):
         key, key1 = jax.random.split(key)
-        params = per_param_mlp_opt.get_params(opt_state)
+        params = per_param_mlp_agg.get_params(opt_state)
         loss, grads = jax.value_and_grad(task.loss)(params, key1, batch)
-        opt_state = per_param_mlp_opt.update(opt_state, grads, loss=loss)
+        opt_state = per_param_mlp_agg.update(opt_state, grads, loss=loss)
 
         return opt_state, key, loss
 
     """Benchmarking"""
 
     optimizers = [
-        ("Adam", adam, adam_update),
-        (per_param_mlp_opt_str, per_param_mlp_opt, per_param_mlp_opt_update),
+        # ("Adam", adam, adam_update),
+        # (per_param_mlp_opt_str, per_param_mlp_opt, per_param_mlp_opt_update),
+        (per_param_mlp_agg_str, per_param_mlp_agg, per_param_mlp_agg_update)
     ]
 
     for opt_str, opt, update in optimizers:
---
+++ mlp_lagg.py
@@ -0,0 +1,185 @@
+from typing import Any, Optional
+import gin
+import haiku as hk
+import jax
+import jax.numpy as jnp
+
+from learned_optimization import summary
+from learned_optimization import tree_utils
+from learned_optimization.learned_optimizers import base as lopt_base
+from learned_optimization.learned_optimizers import common
+from learned_optimization.optimizers import base as opt_base
+
+from learned_optimization.learned_optimizers.mlp_lopt import (
+    PRNGKey,
+    MLPLOptState,
+    _tanh_embedding,
+    _second_moment_normalizer,
+)
+
+
+@gin.configurable
+class MLPLAgg(lopt_base.LearnedOptimizer):
+    """Learned optimizer leveraging a per parameter MLP.
+    This is also known as LOLv2.
+    """
+
+    def __init__(
+        self,
+        exp_mult=0.001,
+        step_mult=0.001,
+        hidden_size=32,
+        hidden_layers=2,
+        compute_summary=True,
+    ):
+        super().__init__()
+        self._step_mult = step_mult
+        self._exp_mult = exp_mult
+        self._compute_summary = compute_summary
+
+        def ff_mod(inp):
+            return hk.nets.MLP([hidden_size] * hidden_layers + [2])(inp)
+
+        self._mod = hk.without_apply_rng(hk.transform(ff_mod))
+
+    def init(self, key: PRNGKey) -> lopt_base.MetaParams:
+        # There are 19 features used as input. For now, hard code this.
+        return self._mod.init(key, jnp.zeros([0, 19]))
+
+    def opt_fn(
+        self, theta: lopt_base.MetaParams, is_training: bool = False
+    ) -> opt_base.Optimizer:
+        decays = jnp.asarray([0.1, 0.5, 0.9, 0.99, 0.999, 0.9999])
+
+        mod = self._mod
+        exp_mult = self._exp_mult
+        step_mult = self._step_mult
+        compute_summary = self._compute_summary
+
+        class _Opt(opt_base.Optimizer):
+            """Optimizer instance which has captured the meta-params (theta)."""
+
+            def init(
+                self,
+                params: lopt_base.Params,
+                model_state: Any = None,
+                num_steps: Optional[int] = None,
+                key: Optional[PRNGKey] = None,
+            ) -> MLPLOptState:
+                """Initialize inner opt state."""
+
+                return MLPLOptState(
+                    params=params,
+                    state=model_state,
+                    rolling_features=common.vec_rolling_mom(decays).init(params),
+                    iteration=jnp.asarray(0, dtype=jnp.int32),
+                )
+
+            def update(
+                self,
+                opt_state: MLPLOptState,
+                grad: Any,
+                loss: float,
+                model_state: Any = None,
+                is_valid: bool = False,
+                key: Optional[PRNGKey] = None,
+            ) -> MLPLOptState:
+                next_rolling_features = common.vec_rolling_mom(decays).update(
+                    opt_state.rolling_features, grad
+                )
+
+                training_step_feature = _tanh_embedding(opt_state.iteration)
+
+                def _update_tensor(p, g, m):
+                    # this doesn't work with scalar parameters, so let's reshape.
+                    if not p.shape:
+                        p = jnp.expand_dims(p, 0)
+                        g = jnp.expand_dims(g, 0)
+                        m = jnp.expand_dims(m, 0)
+                        did_reshape = True
+                    else:
+                        did_reshape = False
+
+                    inps = []
+
+                    # feature consisting of raw gradient values
+                    batch_g = jnp.expand_dims(g, axis=-1)
+                    inps.append(batch_g)
+
+                    # feature consisting of raw parameter values
+                    batch_p = jnp.expand_dims(p, axis=-1)
+                    inps.append(batch_p)
+
+                    # feature consisting of all momentum values
+                    inps.append(m)
+
+                    inp_stack = jnp.concatenate(inps, axis=-1)
+                    axis = list(range(len(p.shape)))
+
+                    inp_stack = _second_moment_normalizer(inp_stack, axis=axis)
+
+                    # once normalized, add features that are constant across tensor.
+                    # namly the training step embedding.
+                    stacked = jnp.reshape(
+                        training_step_feature,
+                        [1] * len(axis) + list(training_step_feature.shape[-1:]),
+                    )
+                    stacked = jnp.tile(stacked, list(p.shape) + [1])
+
+                    inp = jnp.concatenate([inp_stack, stacked], axis=-1)
+
+                    # apply the per parameter MLP.
+                    output = mod.apply(theta, inp)
+
+                    # split the 2 outputs up into a direction and a magnitude
+                    direction = output[..., 0]
+                    magnitude = output[..., 1]
+
+                    # compute the step
+                    step = direction * jnp.exp(magnitude * exp_mult) * step_mult
+                    step = step.reshape(p.shape)
+                    new_p = p - step
+                    if did_reshape:
+                        new_p = jnp.squeeze(new_p, 0)
+
+                    if compute_summary:
+                        for fi, f in enumerate(inp):
+                            summary.summary(
+                                f"mlp_lopt/inp{fi}/mean_abs", jnp.mean(jnp.abs(f))
+                            )
+
+                        avg_step_size = jnp.mean(jnp.abs(step))
+                        summary.summary("mlp_lopt/avg_step_size", avg_step_size)
+
+                        summary.summary(
+                            "mlp_lopt/avg_step_size_hist",
+                            avg_step_size,
+                            aggregation="collect",
+                        )
+
+                        summary.summary(
+                            "mlp_lopt/direction/mean_abs", jnp.mean(jnp.abs(direction))
+                        )
+                        summary.summary(
+                            "mlp_lopt/magnitude/mean_abs", jnp.mean(jnp.abs(magnitude))
+                        )
+                        summary.summary("mlp_lopt/magnitude/mean", jnp.mean(magnitude))
+
+                        summary.summary("mlp_lopt/grad/mean_abs", jnp.mean(jnp.abs(g)))
+
+                    return new_p
+
+                next_params = jax.tree_util.tree_map(
+                    _update_tensor, opt_state.params, grad, next_rolling_features.m
+                )
+                next_opt_state = MLPLOptState(
+                    params=tree_utils.match_type(next_params, opt_state.params),
+                    rolling_features=tree_utils.match_type(
+                        next_rolling_features, opt_state.rolling_features
+                    ),
+                    iteration=opt_state.iteration + 1,
+                    state=model_state,
+                )
+                return next_opt_state
+
+        return _Opt()
