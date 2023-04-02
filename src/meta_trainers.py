39fbb7a284d16a823ffac966a4cc3702a69d1d4b
charles-etienne.joseph <charles-etienne.joseph@login-2.server.mila.quebec>
2023-04-02 17:13:55 -0400

    implemented learned aggregator with adafac mlp

--- src/meta_trainers.py
+++ src/meta_trainers.py
@@ -8,7 +8,41 @@ from learned_optimization.outer_trainers import (
 )
 from learned_optimization.tasks import base as tasks_base
 
+from adafac_mlp_lagg import AdafacMLPLAgg
+from lagg_truncated_step import VectorizedLAggTruncatedStep
 from tasks import get_task
+
+
+def _lagg_meta_trainer(task, num_inner_steps):
+    lagg = AdafacMLPLAgg()
+
+    meta_opt = opt_base.Adam(1e-4)
+
+    def grad_est_fn(task_family):
+        trunc_sched = truncation_schedule.LogUniformLengthSchedule(
+            min_length=100, max_length=num_inner_steps
+        )
+        truncated_step = VectorizedLAggTruncatedStep(
+            task_family,
+            lagg,
+            trunc_sched,
+            num_tasks=16,
+            random_initial_iteration_offset=num_inner_steps,
+        )
+        return truncated_pes.TruncatedPES(
+            truncated_step=truncated_step, trunc_length=50
+        )
+
+    task_family = tasks_base.single_task_to_family(get_task(task))
+    gradient_estimators = [
+        grad_est_fn(task_family),
+    ]
+
+    meta_trainer = gradient_learner.SingleMachineGradientLearner(
+        lagg, gradient_estimators, meta_opt
+    )
+
+    return meta_trainer, "lagg_" + str(lagg.num_grads)
 
 
 def _lopt_meta_trainer(task, num_inner_steps):
@@ -31,9 +65,9 @@ def _lopt_meta_trainer(task, num_inner_steps):
             truncated_step=truncated_step, trunc_length=50
         )
 
-    mlp_task_family = tasks_base.single_task_to_family(get_task(task))
+    task_family = tasks_base.single_task_to_family(get_task(task))
     gradient_estimators = [
-        grad_est_fn(mlp_task_family),
+        grad_est_fn(task_family),
     ]
 
     meta_trainer = gradient_learner.SingleMachineGradientLearner(
@@ -46,6 +80,7 @@ def _lopt_meta_trainer(task, num_inner_steps):
 def get_meta_trainer(optimizer, task, num_inner_steps):
     meta_trainers = {
         "lopt": _lopt_meta_trainer,
+        "lagg": _lagg_meta_trainer,
     }
 
     return meta_trainers[optimizer](task, num_inner_steps)
--- src/optimizers.py
+++ src/optimizers.py
@@ -1,8 +1,46 @@
 import pickle
 
 import jax
+import jax.numpy as jnp
+from haiku._src.data_structures import FlatMap
 from learned_optimization.learned_optimizers import adafac_mlp_lopt
 from learned_optimization.optimizers import nadamw
+
+from adafac_mlp_lagg import AdafacMLPLAgg
+
+
+def _lagg(task):
+    lagg = AdafacMLPLAgg()
+    agg_str = "lagg_" + str(lagg.num_grads)
+    with open(agg_str + ".pickle", "rb") as f:
+        meta_params = pickle.load(f)
+    agg = lagg.opt_fn(meta_params)
+
+    @jax.jit
+    def update(opt_state, key, batch):
+        params = agg.get_params(opt_state)
+        loss = task.loss(params, key, batch)
+
+        def sample_grad_fn(image, label):
+            sub_batch_dict = {}
+            sub_batch_dict["image"] = image
+            sub_batch_dict["label"] = label
+            sub_batch = FlatMap(sub_batch_dict)
+
+            return jax.grad(task.loss)(params, key, sub_batch)
+
+        split_image = jnp.split(batch["image"], lagg.num_grads)
+        split_label = jnp.split(batch["label"], lagg.num_grads)
+        grads = [
+            sample_grad_fn(split_image[i], split_label[i])
+            for i in range(lagg.num_grads)
+        ]
+
+        opt_state = agg.update(opt_state, grads, loss=loss)
+
+        return opt_state, loss
+
+    return agg, agg_str, update
 
 
 def _lopt(task):
@@ -20,7 +58,7 @@ def _lopt(task):
 
         return opt_state, loss
 
-    return (opt, opt_str, update)
+    return opt, opt_str, update
 
 
 def _nadamw(task):
@@ -35,13 +73,14 @@ def _nadamw(task):
 
         return opt_state, loss
 
-    return (opt, opt_str, update)
+    return opt, opt_str, update
 
 
 def get_optimizer(optimizer, task):
     optimizers = {
         "nadamw": _nadamw,
         "lopt": _lopt,
+        "lagg": _lagg,
     }
 
     return optimizers[optimizer](task)
