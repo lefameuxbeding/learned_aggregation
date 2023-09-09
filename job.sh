a878b8c835b20065c64cc16d758e52362c6b385a
Charles-Étienne Joseph <me@charlesetiennejoseph.net>
2023-09-09 19:24:56 -0400

    fedlagg

--- job.sh
+++ job.sh
@@ -4,7 +4,7 @@ module load anaconda/3
 
 conda activate learned_aggregation
 
-python ./src/main.py --config ./config/meta_train/meta_train_fedlopt-adafac.py --name_suffix w --use_pmap --num_devices 2
+python ./src/main.py --config ./config/meta_train/meta_train_fedlagg-adafac.py --name_suffix w --use_pmap --num_devices 2
 
 # test_models_wandb_example.py
 
