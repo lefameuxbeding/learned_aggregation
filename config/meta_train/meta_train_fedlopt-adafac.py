5e9c3c62b44f4b9a2daf834006344ce2a95e251e
Charles-Étienne Joseph <me@charlesetiennejoseph.net>
2023-09-09 18:14:01 -0400

    conv-c10

--- config/meta_train/meta_train_fedlopt-adafac.py
+++ config/meta_train/meta_train_fedlopt-adafac.py
@@ -1,12 +1,12 @@
 _base_ = ["./meta_train_base.py"]
 
 optimizer = "fedlopt-adafac"
-task = "image-mlp-fmst"
+task = "conv-c10"
 
 schedule = dict(
     init_value=3e-10,
-    peak_value=1e-3,
-    end_value=0.333e-3,
+    peak_value=3e-3,
+    end_value=1e-3,
     warmup_steps=100,
     decay_steps=4900,
     exponent=1.0,
