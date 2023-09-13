7100f0ec950d76ea6900c86f98d3f0bfce1c1107
bentherien <benjamintherien@yahoo.ca>
2023-09-12 22:37:36 -0400

    added new config

---
+++ config/meta_train/meta_train_fedlopt-adafac_small-image-mlp-fmst_schedule_6e-3_5000_d6:2.py
@@ -0,0 +1,15 @@
+_base_ = ["./meta_train_base.py"]
+
+num_outer_steps = 5000
+schedule = dict(
+    init_value=3e-10,
+    peak_value=6e-3,
+    end_value=2e-3,
+    warmup_steps=100,
+    decay_steps=4900,
+    exponent=1.0,
+)
+hidden_size = 32
+
+optimizer = "fedlopt-adafac"
+task = "small-image-mlp-fmst"
