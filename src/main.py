d45960359bee17f7d0eb003a53ff73dc79b4cda6
bentherien <benjamintherien@yahoo.ca>
2023-09-18 16:34:15 -0400

    fix

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ def parse_args():
     # fmt: off
     parser.add_argument("--config", type=str, required=True)
     parser.add_argument("--run_type", type=str, choices=["benchmark", "meta-train","sweep"])
-    parser.add_argument("--optimizer", type=str, choices=["sgd,"
+    parser.add_argument("--optimizer", type=str, choices=["sgd",
                                                           "adam", 
                                                           "fedavg", 
                                                           "fedavg-slowmo", 
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     print(xla_bridge.get_backend().platform)
 
     sys.path.append(os.getcwd())
-    os.environ["TFDS_DATA_DIR"] = "/network/scratch/b/benjamin.therien/data/tensorflow_datasets" # os.getenv("SLURM_TMPDIR")
+    # os.environ["TFDS_DATA_DIR"] = "/network/scratch/b/benjamin.therien/data/tensorflow_datasets" # os.getenv("SLURM_TMPDIR")
     os.environ["WANDB_DIR"] = os.getenv("SCRATCH")
 
     args = parse_args()
